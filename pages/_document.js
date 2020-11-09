ef37150a75a49c7d5461201679ca6e6d87dc7ea8
GaryYongYao <garyyongyao@gmail.com>
2020-11-10 00:03:32 +0800

    google verification for search

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta name="google-site-verification" content="wYhahgwKgmrD0ULD-j0SKWx3NwhI4gVZeSpFbpgLQHQ" />
         </Head>
         <body>
           <Main />
